81b7de680f9e0074d48cb13ec5af15a490154a6b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-01 16:15:07 +0900

    initial commt: preact & Typescript

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.dev.config.js');
+
+const rootDir = __dirname;
+const port = process.env['WEB_APP_PORT']
+  ? process.env['WEB_APP_PORT']
+  : 3000;
+const middleware = webpackDevMiddleware(webpack(config), {
+  publicPath: config.output.publicPath,
+});
+
+const app = express();
+app
+  .use(middleware)
+  .get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+  })
+  .use(express.static(rootDir))
+  .listen(port, () => {
+    console.log(`Launching app... http://localhost:${port}\n`);
+  })
+  ;
+
+// Register app and middleware. Required for better performance when running from play.js
+try {
+  pjs.register(app, middleware);
+} catch (err) {
+  console.log(err);
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', 'jsx']
+  },
+  plugins: [],
+}
