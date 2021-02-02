3eb737ed85b324f94243654d4e863636e2e9a412
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-02 21:12:25 +0900

    feat: useRefでインスタンスメソッドを使用

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,15 +1,26 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useState, useRef } from 'preact/hooks';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor as ToastUiEditor } from '@toast-ui/react-editor';
- 
-export const Editor = () => (
-  <ToastUiEditor
-    initialValue="hello react editor world!"
-    previewStyle="vertical"
-    height="600px"
-    initialEditType="markdown"
-    useCommandShortcut={true}
-  />
-);
+
+export const Editor = () => {
+  const editorRef: any = useRef();
+  const handleClick = () => {
+    editorRef.current.getInstance().exec('Bold');
+  };
+  return (
+    <div>
+      <ToastUiEditor
+        initialValue="hello react editor world!"
+        previewStyle="tab"
+        height="600px"
+        initialEditType="markdown"
+        useCommandShortcut={true}
+        ref={editorRef}
+        usageStatistics={false}
+      />
+      <button onClick={handleClick}>make bold</button>
+    </div>
+  );
+};
