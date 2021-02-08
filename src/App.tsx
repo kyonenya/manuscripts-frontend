b3a7c11574fd3400f5d406d1c6760556f13a4b7e
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-08 15:46:36 +0900

    feat: ルーターの導入 簡易的なルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
+import { Router, Route, Link } from 'preact-router';
 import { Article } from './Article';
 import { PageList } from './PageList';
 
 export const App = () => {
   return (
-//    <PageList />
-    <Article />
+    <Fragment>
+      <Link href="/article">Article</Link>
+      <span> / </span>
+      <Link href="/pagelist">PageList</Link>
+      <Router>
+        <Route path="/article" component={Article}></Route>
+        <Route path="/pagelist" component={PageList}></Route>
+      </Router>
+    </Fragment>
   );
 }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
