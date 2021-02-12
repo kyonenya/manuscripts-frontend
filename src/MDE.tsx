bcf6c93fab87fe45d50cf257f8753ccd59bb9251
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-12 13:04:15 +0900

    feat: EasyMDEのインスタンスをuseRefで保持できた！

--- src/MDE.tsx
+++ src/MDE.tsx
@@ -1,5 +1,5 @@
-import { h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { h, Fragment } from 'preact';
+import { useState, useEffect, useRef, Ref } from 'preact/hooks';
 import EasyMDEReact from 'react-simplemde-editor';
 import "easymde/dist/easymde.min.css";
 import { articlable } from './types';
@@ -8,18 +8,23 @@ export const MDE = (props: {
   article: articlable,
   uuid: string,
 }) => {
+  const editorRef: Ref<any> = useRef();
+
   const [text, setText] = useState('');
+
   useEffect(() => {
     setText(props.article.text);
+    editorRef.current.togglePreview();
   }, [props.article]);
-  console.log(text);
 
   return (
-    <EasyMDEReact
-      value={text}
-      //@ts-ignore
-      getMdeInstance={(instance) => instance.togglePreview()}
-      onChange={setText}
-    />
+    <Fragment>
+      <EasyMDEReact
+        value={text}
+        getMdeInstance={(instance) => editorRef.current = instance}
+        onChange={setText}
+      />
+      <button onClick={() => console.log(editorRef.current.value())}>Value</button>
+    </Fragment>
   );
 };
