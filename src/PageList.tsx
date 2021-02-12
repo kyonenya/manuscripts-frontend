e319eb1b8126dcff9f0ea74701e668e1910a0dc5
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-12 18:21:41 +0900

    feat:  ページリストを少しづつsc化

--- src/PageList.tsx
+++ src/PageList.tsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 import { Link } from 'wouter';
+import styled from 'styled-components';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ja';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -9,39 +10,58 @@ import { articlable } from './types';
 dayjs.locale('ja');
 dayjs.extend(relativeTime);
 
+const StyledListItem = styled.li`
+  padding: 0.5em 0;
+  border-bottom: 1px solid hsl(0, 0%, 85%) /* var(monochrome-light) */;
+  display: block;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  color: hsl(0, 0%, 38%) /* var(--monochrome-dark) */;
+`;
+const Footer = styled.footer`
+  margin-top: 0.2em;
+  color: hsl(0, 0%, 38%);
+`;
+
 const PageListItem = (props: {
   article: articlable,
 }) => {
   return (
-    <li class="bl_posts_item" key={props.article.uuid}>
+    <StyledListItem key={props.article.uuid}>
       <Link href={'/?uuid=' + props.article.uuid}>
-        <header class="bl_posts_header">
+        <Header>
           <time class="bl_posts_date">
             {dayjs(props.article.created_at).format('YYYY-MM-DD')}
           </time>
-        </header>
+        </Header>
         <div class="bl_posts_summary">
           <p>
             {props.article.text.substr(0, 125)}
           </p>
         </div>
       </Link>
-      <footer class="bl_posts_footer">
+      <Footer>
         <span class="bl_posts_dateago">{dayjs(props.article.created_at).fromNow()}</span>
         <Link href={'/?uuid=' + props.article.uuid + '&edit=1'}>Edit</Link>
-      </footer>
-    </li>
+      </Footer>
+    </StyledListItem>
   );
 }
+
+const ListContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
 
 export const PageList = (props: {
   articles: articlable[],
 }) => {
   return (
-    <ul class="bl_posts">
+    <ListContainer>
       {props.articles.map(article => 
         <PageListItem article={article} />
       )}
-    </ul>
+    </ListContainer>
   );
 };
