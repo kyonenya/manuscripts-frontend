6acde9f30ba6a1c07b6dbc6c223db9c4b9db6f5a
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-15 15:01:59 +0900

    feat: 新規作成エディタ画面をひとまず実装

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
-import { v4 as uuidv4 } from 'uuid';
 import { Editor } from './Editor';
+import { NewEditor } from './NewEditor';
 import { articlable, emptyArticle } from './types';
 
 export const Article = (props: {
@@ -21,11 +21,10 @@ export const Article = (props: {
 
   return (
     props.isNew
-      ? <h1>New</h1>
+      ? <NewEditor />
       : <Editor article={article} uuid={props.uuid} isEdit={props.isEdit} />
   );
 };
 
-//uuidv4().replace(/-/g, '')
 //7bd2954f3a5c41f09e440e1f9e373f13
 //034392a0ef2f46e4842e249c9bf6dfc5
--- src/NewEditor.tsx
+++ src/NewEditor.tsx
@@ -2,42 +2,30 @@ import { h, Fragment } from 'preact';
 import { useState, useEffect, useRef, Ref } from 'preact/hooks';
 import styled from 'styled-components';
 import EasyMDEReact from 'react-simplemde-editor';
-//import EasyMDE from 'easymde';
 import "./css/easymde.css";
 import "./css/codemirror.css";
+import { v4 as uuidv4 } from 'uuid';
 import dayjs from 'dayjs';
 import { HeaderMenu } from './HeaderMenu';
 import { articlable } from './types';
 
-export const Editor = (props: {
-  article: articlable,
-  uuid: string,
-  isEdit: boolean,
-}) => {
+export const NewEditor = () => {
   const [isLoading, setIsLoading] = useState(false);
   const editorRef: Ref<EasyMDE> = useRef();
-
-  useEffect(() => {
-    // @ts-ignore
-    if (!props.isEdit) editorRef.current.togglePreview();
-  }, []);
-
-  useEffect(() => {
-    editorRef.current.value(props.article.text);
-  }, [props.article]);
 
   const handleSubmit = () => {
     setIsLoading(true);
     const text = editorRef.current.value();
+    const uuid = uuidv4().replace(/-/g, '');
     const article = {
       text,
       tags: ['dummyTag1', 'dummyTag2'],
-      uuid: props.uuid,
+      uuid,
       starred: false,
     };
-    fetch(`https://manuscripts.herokuapp.com/api/entries/${props.uuid}`, {
+    fetch(`https://manuscripts.herokuapp.com/api/entries/${uuid}`, {
 //      method: isNew ? 'POST' : 'PUT',
-      method: 'PUT',
+      method: 'POST',
       body: JSON.stringify(article),
       headers: {
         'Content-type': 'application/json; charset=UTF-8'
@@ -55,9 +43,7 @@ export const Editor = (props: {
     <Fragment>
       <HeaderMenu
         createdAt={
-          props.article.created_at
-            ? dayjs(props.article.created_at).format('YYYY-MM-DD HH:mm')
-            : '...'
+            dayjs().format('YYYY-MM-DD HH:mm')
           }
         handleSubmit={handleSubmit}
         isLoading={isLoading}
@@ -65,7 +51,14 @@ export const Editor = (props: {
       <EditorWrapper>
         <EasyMDEReact
           getMdeInstance={(instance: EasyMDE) => editorRef.current = instance}
-       />
+          options={{
+            autosave: {
+              enabled: true,
+              uniqueId: 'new',
+              delay: 5000,
+            },
+          }}
+        />
       </EditorWrapper>
     </Fragment>
   );
