82e834a50d1ba91ff53d61555c84f97360d09bc4
kyonenya <kyon.musica@gmail.com>
2021-02-17 13:48:23 +0000

    feat: ヘッダーにドロップダウンメニューのひな形を作成

--- src/HeaderMenu.tsx
+++ src/HeaderMenu.tsx
@@ -8,6 +8,8 @@ export const HeaderMenu = (props: {
   handleSubmit: () => void,
   isSubmitting: boolean,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
       <HorizontalList>
@@ -17,7 +19,15 @@ export const HeaderMenu = (props: {
           </Date>
         </li>
         <li><Button>❤️</Button></li>
-        <li><Button>🔖</Button></li>
+        <li onClick={() => setIsOpen(prev => !prev)} style="position: relative;">
+          <Button>🔖</Button>
+          <DropDown isOpen={isOpen}>
+            <div>メニュー1</div>
+            <div>メニュー2</div>
+            <div>メニュー3</div>
+            <div>❌</div>
+          </DropDown>
+        </li>
         <li>
           <Button onClick={props.handleSubmit}>
             { props.isSubmitting ? '⏳' : '✅'}
@@ -34,6 +44,19 @@ const HorizontalList = styled.ul`
   align-items: center /* vertical */;
   list-style-type: none;
   padding: 0px;
+`;
+const DropDown = styled.div`
+  display: ${props => props.isOpen ? 'block' : 'none'};
+  position: absolute;
+  top: 32px;
+  right: 8px;
+  z-index: 99;
+  width: 50vw;
+  max-width: 300px;
+  background: var(--background-color);
+  border-radius: 4px;
+  box-shadow: 0 2px 6px 2px rgba(60, 64, 67, 0.15), 0 1px 2px 0 rgba(60, 64, 67, 0.3);
+  overflow: hidden;
 `;
 const Button = styled.div`
   display: block;
