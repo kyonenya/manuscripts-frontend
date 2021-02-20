8e3270286dd9a00c27945de4202ac0661f225ed8
kyonenya <kyon.musica@gmail.com>
2021-02-20 08:19:25 +0000

    feat: ヘッダーを広めに設定したコンテナに格納

--- src/HeaderMenu.tsx
+++ src/HeaderMenu.tsx
@@ -1,6 +1,7 @@
 import { h, Fragment } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 import styled from 'styled-components';
+import { WideContainer } from './styled/Container';
 import { PlainLink } from './styled/PlainLink';
 import { articlable } from './types';
 
@@ -14,29 +15,31 @@ export const HeaderMenu = (props: {
 
   return (
     <StyledHeaderMenu>
-      <HorizontalList>
-        <li>
-          <Button><PlainLink href="?">↩️</PlainLink></Button>
-        </li>
-        <li><Button>－</Button></li>
-        <li style="margin: 0 auto">
-          <DropDownSwitch onClick={() => setIsOpen(prev => !prev)} isOpen={isOpen}>
-            <Date>{props.createdAt}</Date>
-          </DropDownSwitch>
-          <DropDownBody isOpen={isOpen}>
-            <div>メニュー1</div>
-            <div>メニュー2</div>
-            <div>メニュー3</div>
-            <Button onClick={props.handleDelete}>❌</Button>
-          </DropDownBody>
-        </li>
-        <li><Button>❤️</Button></li>
-        <li>
-          <Button onClick={props.handleSubmit}>
-            { props.isSubmitting ? '⏳' : '✅'}
-          </Button>
-        </li>
-      </HorizontalList>
+      <WideContainer>
+        <HorizontalList>
+          <li>
+            <Button><PlainLink href="?">↩️</PlainLink></Button>
+          </li>
+          <li><Button>－</Button></li>
+          <li style="margin: 0 auto">
+            <DropDownSwitch onClick={() => setIsOpen(prev => !prev)} isOpen={isOpen}>
+              <Date>{props.createdAt}</Date>
+            </DropDownSwitch>
+            <DropDownBody isOpen={isOpen}>
+              <div>メニュー1</div>
+              <div>メニュー2</div>
+              <div>メニュー3</div>
+              <Button onClick={props.handleDelete}>❌</Button>
+            </DropDownBody>
+          </li>
+          <li><Button>❤️</Button></li>
+          <li>
+            <Button onClick={props.handleSubmit}>
+              { props.isSubmitting ? '⏳' : '✅'}
+            </Button>
+          </li>
+        </HorizontalList>
+      </WideContainer>
     </StyledHeaderMenu>
   );
 };
@@ -44,15 +47,17 @@ export const HeaderMenu = (props: {
 export const TopHeaderMenu = () => {
   return (
     <StyledHeaderMenu>
-      <HorizontalList>
-        <li><Button>⚙</Button></li>
-        <li style="margin: 0 auto;">
-          <SearchBox type="text" />
-        </li>
-        <li>
-          <Button><PlainLink href="?new=1">✏️</PlainLink></Button>
-        </li>
-      </HorizontalList>
+      <WideContainer>
+        <HorizontalList>
+          <li><Button>⚙</Button></li>
+          <li style="margin: 0 auto;">
+            <SearchBox type="text" />
+          </li>
+          <li>
+            <Button><PlainLink href="?new=1">✏️</PlainLink></Button>
+          </li>
+        </HorizontalList>
+      </WideContainer>
     </StyledHeaderMenu>
   );
 };
@@ -64,13 +69,14 @@ const StyledHeaderMenu = styled.nav`
   left: 0;
   width: 100vw;
   z-index: 99;
+  background: var(--monochrome-exlight);
+  margin-bottom: 1em;
 `;
 const HorizontalList = styled.ul`
   display: flex;
   justify-content: flex-end;
   align-items: center /* vertical */;
   list-style-type: none;
-  background: var(--monochrome-exlight);
   padding: 1em 0;
   /* reset */
   margin-block-start: 0;
