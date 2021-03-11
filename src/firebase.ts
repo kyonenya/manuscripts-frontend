f33134b5a50b65027c51ad419656b3044567001c
kyonenya <kyon.musica@gmail.com>
2021-03-11 05:13:11 +0000

    feat: process.envのデバッグ

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
+console.log(process.env.FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
